75df5c2d507ee28975df308822c00577f4a3267b
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-07-19 16:17:51 +0500

    add routes

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <h1>Hello from Hammad-Mern-Space</h1>;
-}
-
-export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>Hello from Hammad-Mern-Space</h1>;
+}
+
+export default HomePage;
