bd23d2b29d957bf4e0d79ca5d10b5a6afc247ded
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-07-22 19:11:04 +0500

    add login form along with validation

--- setupTest.ts
+++ setupTest.ts
@@ -1,1 +1,18 @@
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
+import { beforeEach, vi } from "vitest";
+beforeEach(() => {
+  const matchMediaMock = vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }));
+
+  const computedStyledMock = vi.fn().mockImplementation(() => ({}));
+  vi.stubGlobal("matchMedia", matchMediaMock);
+  vi.stubGlobal("computedStyle", computedStyledMock);
+});
