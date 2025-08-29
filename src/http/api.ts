3335148bd85b6ae9b1fd9b8fed5164f9a13a694f
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-08-29 23:22:42 +0500

    add self endpoint

--- src/http/api.ts
+++ src/http/api.ts
@@ -4,5 +4,4 @@ import { api } from "./client";
 // Auth Service
 export const login = (credential: Credentials) =>
   api.post("/auth/login", credential);
-
-
+export const self = () => api.get("/auth/self");
