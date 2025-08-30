5cc0609953b004f46a26f4b1908c729fb56c8918
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-08-30 22:51:48 +0500

    add auto refresh Token, But I think I left something

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 const baseURL = import.meta.env.VITE_BACKEND_API_URL || "http://localhost:5501";
 
@@ -12,3 +13,36 @@ export const api = axios.create({
 });
 
 console.log("Backend API URL:", import.meta.env.VITE_BACKEND_API_URL);
+
+// remove 3 repitions for self if status == 401
+
+const refreshToken = async () => {
+  await axios.post(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._isRetry) {
+      try {
+        originalRequest._isRetry = true;
+
+        const headers = { ...originalRequest.headers };
+        await refreshToken();
+        return api.request({ ...originalRequest, headers });
+      } catch (err) {
+        console.error("Token Refresh error", err);
+        useAuthStore.getState().logout();
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
