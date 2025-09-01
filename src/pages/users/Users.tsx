68c3bf0635bafe277c82e1e577cc278f9bc1c9b5
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-09-01 20:14:27 +0500

    updated users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,51 @@
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Space, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../../http/api";
 import type { User } from "../../types";
+
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+
+  {
+    title: "First Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  // {
+  //   title: "First Name",
+  //   dataIndex: "firstName",
+  //   key: "firstName",
+  //   render: (text: string) => <Link to={`/users/${text}`}>{text}</Link>,
+  // },
+  // {
+  //   title: "Last Name",
+  //   dataIndex: "lastName",
+  //   key: "lastName",
+  // },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
 
 const Users = () => {
   const {
@@ -20,28 +62,30 @@ const Users = () => {
 
   return (
     <>
-      <Breadcrumb
-        separator={<RightOutlined />}
-        items={[
-          { title: <Link to="/">Dashboard</Link> },
-          {
-            title: "Users",
-          },
-        ]}
-      />
-      {isLoading && <div>Loading........</div>}
-      {isError && <div>{error.message}</div>}
-      {users && (
-        <div>
-          <h1>Users</h1>
-
-          <ul>
+      <Space direction="vertical" style={{ width: "100%" }} size="large">
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[
+            { title: <Link to="/">Dashboard</Link> },
+            {
+              title: "Users",
+            },
+          ]}
+        />
+        {isLoading && <div>Loading........</div>}
+        {isError && <div>{error.message}</div>}
+        {users && (
+          <div>
+            <h1>Users</h1>
+            <Table columns={columns} dataSource={users} />;
+            {/* <ul>
             {users.map((user: User) => (
               <li key={user.id}>{user.firstName}</li>
             ))}
-          </ul>
-        </div>
-      )}
+          </ul> */}
+          </div>
+        )}
+      </Space>
     </>
   );
 };
