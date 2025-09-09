e849377c91734e357df8204e9fcdb8e49eadf4dd
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-09-09 12:56:14 +0500

    add filter components for products

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,10 +1,14 @@
-import { Breadcrumb, Flex, Space } from "antd";
-import {  RightOutlined } from "@ant-design/icons";
+import { Breadcrumb, Button, Flex, Form, Space } from "antd";
+import { PlusOutlined, RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import ProductsFilter from "./ProductsFilter";
 
 const Products = () => {
+  const [filterForm] = Form.useForm();
+
   return (
     <>
+      {/* breadcrumb */}
       <Space direction="vertical" style={{ width: "100%" }} size="large">
         <Flex justify="space-between">
           <Breadcrumb
@@ -16,8 +20,15 @@ const Products = () => {
               },
             ]}
           />
-         
         </Flex>
+        {/* filter */}
+        <Form form={filterForm} onFieldsChange={() => {}}>
+          <ProductsFilter>
+            <Button type="primary" icon={<PlusOutlined />} onClick={() => {}}>
+              Add Product
+            </Button>
+          </ProductsFilter>
+        </Form>
       </Space>
     </>
   );
--- src/pages/products/ProductsFilter.tsx
+++ src/pages/products/ProductsFilter.tsx
@@ -1,36 +1,65 @@
-import { Card, Col, Form, Input, Row, Select } from "antd";
+import {
+  Card,
+  Col,
+  Form,
+  Input,
+  Row,
+  Select,
+  Space,
+  Switch,
+  Typography,
+} from "antd";
 
-type UsersFilterProps = {
+type ProductsFilterProps = {
   children?: React.ReactNode;
 };
 
-const UsersFilter = ({ children }: UsersFilterProps) => {
+const ProductsFilter = ({ children }: ProductsFilterProps) => {
   return (
     <Card>
       <Row justify="space-between">
         <Col span={16}>
           <Row gutter={20}>
-            <Col span={8}>
+            <Col span={6}>
               {/* here and there the prop `name` must be the same as in backend or else you will have conflict and things get tighter */}
               <Form.Item name="q">
                 <Input.Search placeholder="Search" allowClear={true} />
               </Form.Item>
             </Col>
-            <Col span={8}>
-              <Form.Item name="role">
+            <Col span={6}>
+              <Form.Item name="rocategoryle">
                 <Select
                   style={{ width: "100%" }}
                   allowClear={true}
-                  placeholder="Select Role"
+                  placeholder="Select Category"
                 >
-                  <Select.Option value="admin">Admin</Select.Option>
-                  <Select.Option value="manager">Manager</Select.Option>
-                  <Select.Option value="customer">Customer</Select.Option>
+                  <Select.Option value="pizza">Pizza</Select.Option>
+                  <Select.Option value="beverage">Beverages</Select.Option>
                 </Select>
               </Form.Item>
             </Col>
 
             {/*  */}
+
+            <Col span={6}>
+              <Form.Item name="resturant">
+                <Select
+                  style={{ width: "100%" }}
+                  allowClear={true}
+                  placeholder="Select Resturant"
+                >
+                  <Select.Option value="Max">Pizza Max</Select.Option>
+                  <Select.Option value="MAMA">Pizza MAMA</Select.Option>
+                  <Select.Option value="Hot">Hot Pizza</Select.Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Space>
+                <Switch defaultChecked onChange={() => {}} />
+                <Typography.Text>Show only published</Typography.Text>
+              </Space>
+            </Col>
 
             {/* <Col span={8}>
               <Select
@@ -55,4 +84,4 @@ const UsersFilter = ({ children }: UsersFilterProps) => {
   );
 };
 
-export default UsersFilter;
+export default ProductsFilter;
