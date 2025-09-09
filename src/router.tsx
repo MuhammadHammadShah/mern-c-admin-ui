ebab3b1d888b2809188c49097a6b2b07b19b13f5
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-09-09 11:29:03 +0500

    add product router

--- src/router.tsx
+++ src/router.tsx
@@ -7,45 +7,47 @@ import NonAuth from "./layouts/NonAuth";
 import CategoryPage from "./pages/CategoryPage";
 import Root from "./layouts/Root";
 import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
 
 export const router = createBrowserRouter([
-
   {
-    path:"/",
-    element:<Root/>,
-    children:[
-      {
-    path: "",
-    element: <Dashboard />,
+    path: "/",
+    element: <Root />,
     children: [
       {
         path: "",
-        element: <HomePage />,
+        element: <Dashboard />,
+        children: [
+          {
+            path: "",
+            element: <HomePage />,
+          },
+          {
+            path: "/users",
+            element: <Users />,
+          },
+          {
+            path: "categories",
+            element: <CategoryPage />,
+          },
+          {
+            path: "products",
+            element: <Products />,
+          },
+        ],
       },
+
       {
-        path: "/users",
-        element: <Users />,
-      },
-      {
-        path: "categories",
-        element: <CategoryPage />,
+        path: "/auth",
+        element: <NonAuth />,
+
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+        ],
       },
     ],
   },
-
-  {
-    path: "/auth",
-    element: <NonAuth />,
-
-    children: [
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-    ],
-  },
-
-    ]
-  }
-  
 ]);
