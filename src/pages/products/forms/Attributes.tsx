a55f09250586e71870b19a6dd36bde413d807754
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-09-11 17:07:55 +0500

    add attributes component ( product form complete )

--- src/pages/products/forms/Attributes.tsx
+++ src/pages/products/forms/Attributes.tsx
@@ -1,9 +1,71 @@
+import { Card, Col, Form, Radio, Row, Switch, Typography } from "antd";
+import type { Category } from "../../../types";
 
+type PricingProp = {
+  selectedCategory: string;
+};
 
-const Attributes = () => {
+const Attributes = ({ selectedCategory }: PricingProp) => {
+  //
+
+  const category: Category | null = selectedCategory
+    ? JSON.parse(selectedCategory)
+    : null;
+
+  if (!category) {
+    return null;
+  }
+
+  //
   return (
-    <div>Attributes</div>
-  )
-}
+    <Card
+      title={<Typography.Text>Product Price</Typography.Text>}
+      bordered={false}
+    >
+      {category.attributes.map((attribute) => {
+        return (
+          <div key={attribute.name}>
+            {attribute.widgetType === "radio" ? (
+              <Form.Item
+                label={attribute.name}
+                name={["attributes", attribute.name]}
+                initialValue={attribute.defaultValue}
+                rules={[
+                  { required: true, message: `${attribute.name} is required` },
+                ]}
+              >
+                <Radio.Group>
+                  {attribute.availableOptions.map((option: string) => {
+                    return (
+                      <Radio.Button value={option} key={option}>
+                        {option}
+                      </Radio.Button>
+                    );
+                  })}
+                </Radio.Group>
+              </Form.Item>
+            ) : attribute.widgetType === "switch" ? (
+              <Row>
+                <Col>
+                  <Form.Item
+                    name={["attributes", attribute.name]}
+                    valuePropName="checked"
+                    initialValue={attribute.defaultValue}
+                    label={attribute.name}
+                  >
+                    <Switch
+                      checkedChildren="Yes"
+                      unCheckedChildren="No"
+                    ></Switch>
+                  </Form.Item>
+                </Col>
+              </Row>
+            ) : null}
+          </div>
+        );
+      })}
+    </Card>
+  );
+};
 
-export default Attributes
+export default Attributes;
