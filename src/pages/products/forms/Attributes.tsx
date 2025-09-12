f77739acebcda364ac944d02e6d10876411117c4
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-09-12 10:49:33 +0500

    add attributes in product form (pre commit is correct but commit message was a mistake :D )

--- src/pages/products/forms/Attributes.tsx
+++ src/pages/products/forms/Attributes.tsx
@@ -1,67 +1,62 @@
 import { Card, Col, Form, Radio, Row, Switch, Typography } from "antd";
 import type { Category } from "../../../types";
 
-type PricingProp = {
+type PricingProps = {
   selectedCategory: string;
 };
 
-const Attributes = ({ selectedCategory }: PricingProp) => {
-  //
-
+const Attributes = ({ selectedCategory }: PricingProps) => {
   const category: Category | null = selectedCategory
     ? JSON.parse(selectedCategory)
     : null;
 
-  console.log("Parsed category:", category);
-  console.log("Category attributes:", category?.attributes);
-
-  if (!category) {
+  if (!category || !Array.isArray(category.attributes)) {
     return null;
   }
 
-  //
   return (
     <Card
-      title={<Typography.Text>Product Price</Typography.Text>}
+      title={<Typography.Text>Attributes</Typography.Text>}
       bordered={false}
     >
-      {category.attributes.map((attribute) => {
-        return (
-          <div key={attribute.name}>
-            {attribute.widgetType === "radio" ? (
-              <Form.Item
-                label={attribute.name}
-                name={["attributes", attribute.name]}
-                initialValue={attribute.defaultValue}
-                rules={[
-                  { required: true, message: `${attribute.name} is required` },
-                ]}
-              >
-                <Radio.Group>
-                  {attribute.availableOptions?.map((option: string) => (
-                    <Radio.Button value={option} key={option}>
-                      {option}
-                    </Radio.Button>
-                  ))}
-                </Radio.Group>
-              </Form.Item>
-            ) : attribute.widgetType === "switch" ? (
-              <Row>
-                <Col>
-                  <Form.Item
-                    name={["attributes", attribute.name]}
-                    valuePropName="checked"
-                    initialValue={attribute.defaultValue}
-                    label={attribute.name}
-                  >
-                    <Switch checkedChildren="Yes" unCheckedChildren="No" />
-                  </Form.Item>
-                </Col>
-              </Row>
-            ) : null}
-          </div>
-        );
-      })}
+      {category.attributes.map((attribute) => (
+        <div key={attribute.name}>
+          {attribute.widgetType === "radio" ? (
+            <Form.Item
+              label={attribute.name}
+              name={["attributes", attribute.name]}
+              initialValue={attribute.defaultValue}
+              rules={[
+                {
+                  required: true,
+                  message: `${attribute.name} is required`,
+                },
+              ]}
+            >
+              <Radio.Group>
+                {attribute.availableOptions?.map((option) => (
+                  <Radio.Button value={option} key={option}>
+                    {option}
+                  </Radio.Button>
+                ))}
+              </Radio.Group>
+            </Form.Item>
+          ) : attribute.widgetType === "switch" ? (
+            <Row>
+              <Col>
+                <Form.Item
+                  name={["attributes", attribute.name]}
+                  valuePropName="checked"
+                  initialValue={attribute.defaultValue}
+                  label={attribute.name}
+                >
+                  <Switch checkedChildren="Yes" unCheckedChildren="No" />
+                </Form.Item>
+              </Col>
+            </Row>
+          ) : null}
+        </div>
+      ))}
     </Card>
   );
 };
